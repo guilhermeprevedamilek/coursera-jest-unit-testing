bd8cc3e1ae61e4a1f074e0a315cc222ea4b16137
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-12 15:35:24 -0300

    test: start using more matchers

--- functions.js
+++ functions.js
@@ -2,6 +2,8 @@ const functions = {
   getAge: (name, age) => {
     return `${name} is ${age} years old`
   },
+
+  add: (num1, num2) => num1 + num2,
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,14 @@ const functions = require('./functions')
 test('is defined', () => {
   expect(functions.getAge('Rudi', 35)).toBeDefined()
 })
+
+test('2 + 2 is equal to 4', () => {
+  expect(functions.add(2, 2)).toBe(4)
+})
+
+test('object assignment', () => {
+  const data = { one: 1 }
+  data['two'] = 2
+
+  expect(data).toEqual({ one: 1, two: 2 })
+})

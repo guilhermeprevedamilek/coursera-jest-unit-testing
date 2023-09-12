3d0cac2eb673b3fadc86ca33abad2d78c19ec671
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-12 15:53:00 -0300

    test: using even more matchers to test different data types and structures

--- functions.js
+++ functions.js
@@ -7,6 +7,9 @@ const functions = {
 
   isNull: () => null,
   isUndefined: () => undefined,
+  compileAndroidCode: () => {
+    throw new Error('you are using the wrong JDK')
+  },
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -31,3 +31,36 @@ test('zero', () => {
   expect(z).not.toBeTruthy()
   expect(z).toBeFalsy()
 })
+
+test('two plus two', () => {
+  const value = 2 + 2
+
+  expect(value).toBeGreaterThan(3)
+  expect(value).toBeGreaterThanOrEqual(3.5)
+  expect(value).toBeLessThan(5)
+  expect(value).toBeLessThanOrEqual(4.5)
+  expect(value).toBe(4)
+  expect(value).toEqual(4)
+})
+
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2
+
+  expect(value).toBeCloseTo(0.3)
+})
+
+test('there is no I in team ', () => {
+  expect('team').not.toMatch(/I/)
+})
+
+test('the shopping list has beer on it', () => {
+  const shoppingList = ['nappies', 'toilet roll', 'bin bags', 'beer']
+
+  expect(shoppingList).toContain('beer')
+  expect(new Set(shoppingList)).toContain('beer')
+})
+
+test('compiling android goes as expected', () => {
+  expect(functions.compileAndroidCode).toThrow()
+  expect(functions.compileAndroidCode).toThrow(/JDK/)
+})
